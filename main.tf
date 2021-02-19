4bd087870809a747c05194c1762c34b7a4a56af5
Uwannah, Michael <m.uwannah@ambifox.de>
2021-02-19 22:42:18 +0100

    remove token variable

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-# Set the variable value in *.tfvars file
-# or using the -var="hcloud_token=..." CLI option
-variable "hcloud_token" {}
 
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  token = "${var.hcloud_token}"
 }
 
 # Create a server
