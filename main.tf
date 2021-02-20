0ad3ea17a3f000a1a64215b0b398f8007fc97b2b
Uwannah, Michael <m.uwannah@ambifox.de>
2021-02-20 20:19:20 +0100

    remove worker

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ resource "hcloud_server" "k8s-worker-1" {
   location    = "nbg1"
 }
 
-resource "hcloud_server" "k8s-worker-2" {
-  name        = "k8s-worker-2"
-  image       = var.ubuntu_image
-  server_type = "cx21"
-  location    = "nbg1"
+# resource "hcloud_server" "k8s-worker-2" {
+#   name        = "k8s-worker-2"
+#   image       = var.ubuntu_image
+#   server_type = "cx21"
+#   location    = "nbg1"
 
-}
+# }
