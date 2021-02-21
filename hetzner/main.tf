7cacb3363fabde7f56237f7fc8cf50a6a9f29a46
Uwannah, Michael <m.uwannah@ambifox.de>
2021-02-21 14:22:30 +0100

    add worker 3

--- hetzner/main.tf
+++ hetzner/main.tf
@@ -11,17 +11,23 @@ resource "hcloud_server" "k8s-master-1" {
   location    = "nbg1"
 }
 
-# resource "hcloud_server" "k8s-worker-1" {
-#   name        = "k8s-worker-1"
-#   image       = var.ubuntu_image
-#   server_type = "cx21"
-#   location    = "nbg1"
-# }
+resource "hcloud_server" "k8s-worker-1" {
+  name        = "k8s-worker-1"
+  image       = var.ubuntu_image
+  server_type = "cx21"
+  location    = "nbg1"
+}
 
-# resource "hcloud_server" "k8s-worker-2" {
-#   name        = "k8s-worker-2"
-#   image       = var.ubuntu_image
-#   server_type = "cx21"
-#   location    = "nbg1"
+resource "hcloud_server" "k8s-worker-2" {
+  name        = "k8s-worker-2"
+  image       = var.ubuntu_image
+  server_type = "cx21"
+  location    = "nbg1"
+}
 
-# }
+resource "hcloud_server" "k8s-worker-3" {
+  name        = "k8s-worker-3"
+  image       = var.ubuntu_image
+  server_type = "cx21"
+  location    = "nbg1"
+}
