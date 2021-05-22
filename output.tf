3cfcc04fd8f80d12076a8e87df23109ece0ed86c
Uwannah, Michael <m.uwannah@ambifox.de>
2021-05-22 13:07:34 +0200

    test output cluster net

--- output.tf
+++ output.tf
@@ -1,6 +1,11 @@
+output "env-dynamic-url" {
+  value = "test.cluster.net"
+}
+
 output "vm_ip_addresses" {
   value = {
     for vm in hcloud_server.k8s-worker:
     vm.name => vm.ipv4_address
   }
+
 }
